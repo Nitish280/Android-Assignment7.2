93722cb6c03fcb16706bf4a12aa32b6be0c25615
Nitish280 <nsrccs@gmail.com>
2017-07-16 19:55:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/nitishsingh/andriod72/MainActivity.java
+++ app/src/main/java/com/example/nitishsingh/andriod72/MainActivity.java
@@ -9,19 +9,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    // declaration
     Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//here we are creating the main method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=(Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View view) { //here we are creating the on click method
+               
+                //here we are using intent to go the another screen
                 Intent intent=new Intent(Intent.ACTION_PICK,ContactsContract.Contacts.CONTENT_URI);
+                //here we are satarting the activity
                 startActivity(intent);
 
 
